b67b4c06591951017d8975a674909ad89be47837
NONOThingC <14366189@qq.com>
2019-04-01 17:00:05 +0800

    验证码样式，表格样式修改

--- GalaX/login/forms.py
+++ GalaX/login/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from captcha.fields import CaptchaField
-
+from captcha.fields import CaptchaTextInput
 class UserForm(forms.Form):
     username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    captcha = CaptchaField(label='验证码')
+    captcha = CaptchaField(label='验证码', widget=CaptchaTextInput(attrs={'class': 'form-control'}))
 
 class RegisterForm(forms.Form):
     gender = (
@@ -17,4 +17,4 @@ class RegisterForm(forms.Form):
     phone_number = forms.CharField(label="电话号码", max_length=15, widget=forms.NumberInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label="邮箱地址", widget=forms.EmailInput(attrs={'class': 'form-control'}))
     sex = forms.ChoiceField(label='性别', choices=gender)
-    captcha = CaptchaField(label='验证码')
+    captcha = CaptchaField(label='验证码', widget=CaptchaTextInput(attrs={'class': 'form-control'}))
